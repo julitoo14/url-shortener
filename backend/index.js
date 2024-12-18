5822bad7d5c622b561c42f8cce515acfd9aee277
julitoo14 <juuligarcia2208@gmail.com>
2024-12-18 00:00:29 +0000

    deploy

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const connection = require('./database/connection');
 require('dotenv').config();
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 connection();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'dist')));
 
 
 //routes
 app.use('/', require('./routes/url.routes'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
 
 app.listen(port, () => {
     console.log(`El servidor está escuchando en el puerto ${port}`);
